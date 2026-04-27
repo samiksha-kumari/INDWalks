515c3cbc704548a364ada91480d97f6eeb9e37b6
Samiksha Kumari <samiksha.kumari1@kyndryl.com>
2026-04-27 14:46:38 +0530

    implemented create region

--- INDWalks.API/Controllers/RegionsController.cs
+++ INDWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using INDWalks.API.Data;
+using INDWalks.API.Models.Domain;
 using INDWalks.API.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -41,7 +42,7 @@ namespace INDWalks.API.Controllers
         //get region by ID
         [HttpGet]
         [Route("{id:Guid}")]  // get mapped from route to a input parameter
-        public IActionResult GetRegionById([FromRoute] Guid id)
+        public IActionResult GetById([FromRoute] Guid id)
         {
             //var region =  dbContext.Regions.Find(id);
             var regionDoamin = dbContext.Regions.FirstOrDefault(x => x.Id == id);
@@ -60,6 +61,35 @@ namespace INDWalks.API.Controllers
             };
 
             return Ok(regionDTO);
+        }
+
+        //Post to create new region
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO to Domain Model
+            var regionDomailModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+
+            //Use DomainModel to create Region
+
+            dbContext.Regions.Add(regionDomailModel);
+            dbContext.SaveChanges(); // EF saves the changes and it will reflected in SQL server.
+
+            //Map Domain Model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomailModel.Id,
+                Name = regionDomailModel.Name,
+                Code = regionDomailModel.Code,
+                RegionImageUrl = regionDomailModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
         }
     }
 }
